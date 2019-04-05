51dab101c979c81aae9b4873e3e3a26a2f7240da
Krishna Kumar Dey <krishnakumar.d16@iiits.in>
2019-04-06 01:56:58 +0530

    remove result url

--- Api/app.py
+++ Api/app.py
@@ -5,7 +5,6 @@ from sentence import Search,Add_Doc,TestResource
 
 search = Search()
 add_doc = Add_Doc()
-result = Result()
 test_resource = TestResource()
 # Add a route to serve the resource
 api.add_route('/test',test_resource)
